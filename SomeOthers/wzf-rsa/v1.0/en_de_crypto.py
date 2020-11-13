8655d7f1d3f5ebf4115843d69d6c0db56690a2d2
LittleFiveLee <2535904526@qq.com>
2020-11-13 15:47:28 +0800

    xxx

--- SomeOthers/wzf-rsa/v1.0/en_de_crypto.py
+++ SomeOthers/wzf-rsa/v1.0/en_de_crypto.py
@@ -34,7 +34,7 @@ data = data + "key" + "sysid" + str(time.time())
 #data = "{\"0\":\"0\",\"1\":\"1\",\"10\":\"10\",\"11\":\"11\",\"12\":\"12\",\"13\":\"13\",\"14\":\"14\",\"15\":\"15\",\"16\":\"16\",\"17\":\"17\",\"18\":\"18\",\"19\":\"19\",\"2\":\"2\",\"3\":\"3\",\"4\":\"4\",\"5\":\"5\",\"6\":\"6\",\"7\":\"7\",\"8\":\"8\",\"9\":\"9\"}"
 
 """生成证书的存放路径"""
-path = "/home/dongweili/LDW/mytest/git_spider/SomeOthers/wzf-rsa"
+path = "xxxxx"
 with open(path + "/client-public.pem") as f_pub_en:
     pub_key = f_pub_en.read()
     print(pub_key)
