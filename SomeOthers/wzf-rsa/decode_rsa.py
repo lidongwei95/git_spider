61e6144b3adaaedf046a4991cf7a65510503d968
LittleFiveLee <2535904526@qq.com>
2020-11-16 09:23:41 +0800

    增加readme内容，删除隐私秘钥

--- SomeOthers/wzf-rsa/decode_rsa.py
+++ SomeOthers/wzf-rsa/decode_rsa.py
@@ -12,7 +12,7 @@ from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA
 import base64
-encrypt_text ="S1NckUAKDrp1+5qtoq4fesaGZrP/RGnwizvtxRoQ6cYi6do5vHH/1l6mg3MuasJinatNYpErJvCX1NyDjM088W6tzmksDLoikt2ZGakvF9lmo12rrG4ENERZOoN8SgjGQi3G+C4jwOa+3Lc0NYPjday/aCn9iOLTee738weHyfVOz/J6YkTqRoVUZ3kOODjiLTugSX0oSEzqSGi2VZ2OsTpAeSwiG9eztpGO3Z6ix3yKDu7yZplUlKXnK0jyUrXeiUF+gbDSrxPbw8uHzraRQRv5XsvbDPQGvKn6vLZR3jQiiFFbJ+Vk3gDy3HbRyXH/fFd471u9/1cso2BTdi6iJQ==" 
+encrypt_text ="xxx" 
 with open("client-private.pem") as f:
     key = f.read()
     rsakey = RSA.importKey(key)
